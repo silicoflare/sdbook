c0032c6a3b54ddb56a677b56e4c430fd37d7bfa1
Suraj B M <silicoflare@gmail.com>
2024-08-04 00:16:24 +0530

    fix: smol things

--- src/pages/_components/NoteCard.tsx
+++ src/pages/_components/NoteCard.tsx
@@ -5,7 +5,7 @@ import moment from "moment";
 import Link from "next/link";
 
 export default function NoteCard({ note }: { note: Note }) {
-  return (
+  return note ? (
     <Link
       href={`/notes/${note.noteID}`}
       className="flex w-full flex-col rounded-md border border-primary bg-primary p-5 text-primary-foreground gap-3 transition duration-200 ease-in-out hover:scale-110 cursor-pointer"
@@ -13,5 +13,5 @@ export default function NoteCard({ note }: { note: Note }) {
       <h2 className="text-2xl">{note.title}</h2>
       <span>{moment(note.updatedAt).format("LL \\at LT")}</span>
     </Link>
-  );
+  ) : null;
 }
